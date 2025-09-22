b29c6e491135552350f6467fdb0532f8c4f9f20e
Amilie <amilie@allthingslinux.org>
2025-09-22 10:20:54 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ArchForAll Bootstrap.
+# No NVIDIA.
+
+# Define your mirror URL here
+MIRROR_URL="https://raw.githubusercontent.com/AmilieCoding/arch-for-all/refs/heads/main/afa-gnome-x86_64-nogui-alpha-0-0-1.py"
+
+# Download and run
+curl -fsSL "$MIRROR_URL" | python3
